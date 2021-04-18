01b64966b1c8a860bd0c900665b98821a9f4c5d7
Anton Heestand <anton.heestand@gmail.com>
2021-04-18 18:43:07 +0200

    Semi Select

--- Sources/Logger/Logger.swift
+++ Sources/Logger/Logger.swift
@@ -31,20 +31,57 @@ public struct Logger {
         }
     }
     
+    struct Log {
+        let level: Level
+        let message: String?
+        let filePath: String
+        let funcName: String
+    }
+    
+    public class PreLog {
+        let log: Log
+        var cancelled: Bool = false
+        init(log: Log) {
+            self.log = log
+            DispatchQueue.main.async {
+                guard !self.cancelled else { return }
+                Logger.log(log, timeout: true)
+            }
+        }
+        func cancel() {
+            cancelled = true
+        }
+    }
+    
+    public static func preLog(level: Level = .info, message: String? = nil, filePath: String = #file, funcName: String = #function) -> PreLog {
+        PreLog(log: Log(level: level, message: message, filePath: filePath, funcName: funcName))
+    }
+    
+    public static func postLog(_ preLog: PreLog) {
+        preLog.cancel()
+        log(preLog.log)
+    }
+    
     public static func log(level: Level = .info, message: String? = nil, filePath: String = #file, funcName: String = #function) {
-        
+        log(Log(level: level, message: message, filePath: filePath, funcName: funcName))
+    }
+    private static func log(_ log: Log, timeout: Bool = false) {
+
 //        let fileName: String = String(filePath.split(separator: "/").last?.split(separator: ".").first ?? "")
-        let fileName: String = filePath.components(separatedBy: "Sources/").last ?? ""
+        let fileName: String = log.filePath.components(separatedBy: "Sources/").last ?? ""
         
-        var log: String = "\(prefix) \(level.label) \(fileName) \(funcName)"
+        var text: String = "\(prefix) \(log.level.label) \(fileName) \(log.funcName)"
         
-        if let message: String = message {
-            log += " \"\(message)\""
+        if let message: String = log.message {
+            text += " \"\(message)\""
         }
-        if let error: Error = level.error {
-            log += " Error: \(String(describing: error))"
+        if let error: Error = log.level.error {
+            text += " Error: \(String(describing: error))"
+        }
+        if timeout {
+            text += " [TIMEOUT]"
         }
         
-        print(log)
+        print(text)
     }
 }
